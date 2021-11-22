448b1339bfb167731b02cee02f522560900ab9b8
Hazeer Zacky <contact.hazky@gmail.com>
2021-11-22 16:26:40 +0530

    Validation for Add post

--- app/src/main/java/com/example/thagavaltimes/AddActivity.java
+++ app/src/main/java/com/example/thagavaltimes/AddActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.RegexValidator;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class AddActivity extends AppCompatActivity {
 
     EditText title, category, description, purl;
     Button btnAdd;
+    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +38,39 @@ public class AddActivity extends AppCompatActivity {
         category = (EditText)findViewById(R.id.txtCategory);
         description = (EditText)findViewById(R.id.txtDescription);
         purl = (EditText)findViewById(R.id.txtPurl);
-
         btnAdd = (Button)findViewById(R.id.btnAdd);
+
+        //            ------------------ [ Validation  Part Start ] ------------------
+
+        //Initialize Validation Style
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //Add Validation for Title
+        awesomeValidation.addValidation(this, R.id.txtTitle,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        //Add Validation for Category
+        awesomeValidation.addValidation(this, R.id.txtCategory,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_category);
+
+        //Add Validation for Description
+        awesomeValidation.addValidation(this, R.id.txtDescription,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_description);
+
+        //Add Validation for Description
+        awesomeValidation.addValidation(this, R.id.txtPurl,
+                Patterns.WEB_URL,R.string.invalid_url);
+
+        //            ------------------ [ Validation  Part End ] ------------------
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                insertData();
-                clearAll();
+                if (awesomeValidation.validate()){
+                    insertData();
+                    clearAll();
+                }
             }
         });
 
